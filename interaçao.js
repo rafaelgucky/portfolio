add7ff1453de57f2e2f1431779bc42331311c26b
rafaelgucky <rafaelgucky97@gmail.com>
2022-09-25 14:48:09 -0300

    fazendo

--- interaçao.js
+++ interaçao.js
@@ -1,4 +1,7 @@
 var titulo = window.document.getElementById('titulo')
 var nome = window.prompt('Qual é o seu nome?')
-
-titulo.innerHTML = `Olá ${nome} bem-vindo ao meu potfólio!`
+if(nome == null){
+    titulo.innerHTML = `Olá, bem-vindo ao meu potfólio!`
+}else{
+titulo.innerHTML = `Olá ${nome}, bem-vindo ao meu potfólio!`
+}
